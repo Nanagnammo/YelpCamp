426c7cc12723ddd0d078e5d8de409bd267f0f82d
Nanagnammo <gentilejacopo@hotmail.it>
2022-09-30 15:20:05 +1000

    Cluster Map

--- seeds/index.js
+++ seeds/index.js
@@ -16,7 +16,7 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () =>{
     await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 300; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
@@ -28,32 +28,35 @@ const seedDB = async () =>{
             price,
             geometry:{
                 type: "Point",
-                coordinates: [14.248783, 40.835934]
+                coordinates: [
+                  cities[random1000].longitude,
+                  cities[random1000].latitude
+                ]
             },
             images: [
                 {
-                  url: 'https://res.cloudinary.com/spacemicrowave/image/upload/v1662991623/YelpCamp/mbrpybrhhw2gf4p15eoc.jpg',
-                  filename: 'YelpCamp/mbrpybrhhw2gf4p15eoc'
+                    url: 'https://res.cloudinary.com/spacemicrowave/image/upload/v1662991623/YelpCamp/mbrpybrhhw2gf4p15eoc.jpg',
+                    filename: 'YelpCamp/mbrpybrhhw2gf4p15eoc'
                 },
                 {
-                  url: 'https://res.cloudinary.com/spacemicrowave/image/upload/v1662991629/YelpCamp/lhmn2a7r4ajf2yogk4bi.jpg',
-                  filename: 'YelpCamp/lhmn2a7r4ajf2yogk4bi'
+                     url: 'https://res.cloudinary.com/spacemicrowave/image/upload/v1662991629/YelpCamp/lhmn2a7r4ajf2yogk4bi.jpg',
+                    filename: 'YelpCamp/lhmn2a7r4ajf2yogk4bi'
                 },
                 {
-                  url: 'https://res.cloudinary.com/spacemicrowave/image/upload/v1662991630/YelpCamp/igskehaustjiotz6rekq.jpg',
-                  filename: 'YelpCamp/igskehaustjiotz6rekq'
+                    url: 'https://res.cloudinary.com/spacemicrowave/image/upload/v1662991630/YelpCamp/igskehaustjiotz6rekq.jpg',
+                    filename: 'YelpCamp/igskehaustjiotz6rekq'
                 },
                 {
-                  url: 'https://res.cloudinary.com/spacemicrowave/image/upload/v1662991639/YelpCamp/i1okj4xxqpuazkdfijlu.jpg',
-                  filename: 'YelpCamp/i1okj4xxqpuazkdfijlu'
+                    url: 'https://res.cloudinary.com/spacemicrowave/image/upload/v1662991639/YelpCamp/i1okj4xxqpuazkdfijlu.jpg',
+                    filename: 'YelpCamp/i1okj4xxqpuazkdfijlu'
                 },
                 {
-                  url: 'https://res.cloudinary.com/spacemicrowave/image/upload/v1662991644/YelpCamp/snwiritw3ioqjuc4leg6.jpg',
-                  filename: 'YelpCamp/snwiritw3ioqjuc4leg6'
+                    url: 'https://res.cloudinary.com/spacemicrowave/image/upload/v1662991644/YelpCamp/snwiritw3ioqjuc4leg6.jpg',
+                    filename: 'YelpCamp/snwiritw3ioqjuc4leg6'
                 },
                 {
-                  url: 'https://res.cloudinary.com/spacemicrowave/image/upload/v1662991650/YelpCamp/l6arfbcyarlnkdowkdfd.jpg',
-                  filename: 'YelpCamp/l6arfbcyarlnkdowkdfd'
+                    url: 'https://res.cloudinary.com/spacemicrowave/image/upload/v1662991650/YelpCamp/l6arfbcyarlnkdowkdfd.jpg',
+                    filename: 'YelpCamp/l6arfbcyarlnkdowkdfd'
                 }
               ]
         });
